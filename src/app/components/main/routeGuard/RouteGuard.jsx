ac581514325395ebd21e871859201e603365492a
pobuchow <pobu1@hotmail.com>
2021-04-07 19:18:08 +0200

    RouteGuard cleanup

--- src/app/components/main/routeGuard/RouteGuard.jsx
+++ src/app/components/main/routeGuard/RouteGuard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Redirect } from 'react-router';
 import { store } from '../../../store';
 
@@ -8,17 +7,7 @@ function withData(Component) {
     if (!store.getState().session.authenticated) {
       return <Redirect to="/" />;
     }
-    return <Component match={match} />;
-  };
-  redirect.propTypes = {
-    match: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      event_datetime: PropTypes.instanceOf(Date).isRequired,
-      homeTeam: PropTypes.string.isRequired,
-      awayTeam: PropTypes.string.isRequired,
-      goalsHomeTeam: PropTypes.number,
-      goalsAwayTeam: PropTypes.number,
-    }),
+    return <Component route={match} />;
   };
   return redirect;
 }
