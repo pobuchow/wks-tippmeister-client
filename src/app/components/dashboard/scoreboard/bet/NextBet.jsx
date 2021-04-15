c510b4364d5f4f4f08faeb2b309ce5b20c3875b7
pobuchow <pobu1@hotmail.com>
2021-04-15 20:00:10 +0200

    refactor nextbet component

--- src/app/components/dashboard/scoreboard/bet/NextBet.jsx
+++ src/app/components/dashboard/scoreboard/bet/NextBet.jsx
@@ -1,38 +1,54 @@
-import React from "react";
-import { connect } from "react-redux";
-import _ from "lodash";
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 import {
   getNextMatch,
-} from "../../../../services/match/MatchService";
+} from '../../../../services/match/MatchService';
 
-export const NextBet = ({ betId, goalsHomeTeam, goalsAwayTeam}) => betId ? (
-  <div style={{display: 'inline-block'}}>
-    {betId ? 
+export const NextBet = ({ betId, goalsHomeTeam, goalsAwayTeam }) => (betId ? (
+  <div style={{ display: 'inline-block' }}>
+    {betId
+      ? (
         <div key={betId}>
-            {goalsHomeTeam} : {goalsAwayTeam}
-        </div> 
-    : ""}
+          {goalsHomeTeam}
+          {' '}
+          :
+          {' '}
+          {goalsAwayTeam}
+        </div>
+      )
+      : ''}
   </div>
-) : null;
+) : null);
 
 function mapState2Props(state, ownProps) {
-  const game = _.find(state.games, { 'id': ownProps.gameId });
-  let matches =_.filter(state.matches, function (match) {
-    return _.includes(game.matches, match.id);
-  });
-  let nextMatch = getNextMatch(matches);
-  if(!nextMatch){
+  const game = _.find(state.games, { id: ownProps.gameId });
+  const matches = _.filter(state.matches, (match) => _.includes(game.matches, match.id));
+  const nextMatch = getNextMatch(matches);
+  if (!nextMatch) {
     return {};
   }
-  let nextBets =_.filter(state.bets, function (bet) {
-    return bet.game === game.id && bet.match === nextMatch.id;
-  });
-  let bet = _.find(nextBets, ["owner", ownProps.userId]);
+  const nextBets = _.filter(state.bets,
+    (bet) => bet.game === game.id && bet.match === nextMatch.id);
+  const bet = _.find(nextBets, ['owner', ownProps.userId]);
   return bet ? {
     betId: bet.id,
     goalsHomeTeam: bet.goalsHomeTeam,
-    goalsAwayTeam: bet.goalsAwayTeam
+    goalsAwayTeam: bet.goalsAwayTeam,
   } : {};
 }
+
+NextBet.propTypes = {
+  betId: PropTypes.string,
+  goalsHomeTeam: PropTypes.number,
+  goalsAwayTeam: PropTypes.number,
+};
+
+NextBet.defaultProps = {
+  betId: null,
+  goalsHomeTeam: null,
+  goalsAwayTeam: null,
+};
 
 export const ConnectedNextBet = connect(mapState2Props)(NextBet);
